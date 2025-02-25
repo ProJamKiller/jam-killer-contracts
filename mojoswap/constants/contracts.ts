ce36894c9f42b81f495a83f3336c8a4754e080f0
projamkiller <fletcher@jamkillerproductions.io>
2025-02-25 17:38:40 -0600

    Commit

--- mojoswap/constants/contracts.ts
+++ mojoswap/constants/contracts.ts
@@ -1,4 +1,4 @@
-export const PJK_TOKEN_ADDRESS = "0xfc0e40e7d6aba1079409966bcacb2273f048da5f"; // Proxy address
-export const MOJO_TOKEN_ADDRESS = "0xYourMojoTokenAddressHere";
-export const PJK_BURNER_ADDRESS = "0xYourBurnerAddressHere";
-export const MOJO_SWAP_CONTRACT = "0xYourSwapContractAddressHere";
+export const PJK_TOKEN_ADDRESS = "0xfc0E40e7D6AbA1079409966bcAcB2273f048Da5F";
+export const MOJO_SWAP_CONTRACT = "0xfb8ed158d1c0998b028d1b087d1891f93adf4ed2";
+export const PJK_BURNER_ADDRESS = "0x42c2bb9bc4642ef47cacb23a88646c7a0ad879a5";
+export const MOJO_TOKEN_ADDRESS = "0xf9e7d3cd71ee60c7a3a64fa7fcb81e610ce1daa5";
