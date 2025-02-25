2dc71236254a03e601b0b48baeece589801fccf3
projamkiller <fletcher@jamkillerproductions.io>
2025-02-25 15:14:04 -0600

    Commit

--- mojoswap/constants/contracts.ts
+++ mojoswap/constants/contracts.ts
@@ -1,3 +1,3 @@
-export const MOJO_SWAP_CONTRACT = "0xf9e7d3cd71ee60c7a3a64fa7fcb81e610ce1daa5";
+export const MOJO_SWAP_CONTRACT = "0xfb8ed158d1c0998b028d1b087d1891f93adf4ed2";
 export const MOJO_TOKEN_ADDRESS = "0xf9e7d3cd71ee60c7a3a64fa7fcb81e610ce1daa5";
 export const PJK_BURNER_ADDRESS = "0x42c2bb9bc4642ef47cacb23a88646c7a0ad879a5";
